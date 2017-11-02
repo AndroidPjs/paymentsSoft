6230b49a621721ad2b69479b83fc6663e373751f
TheoRev <hrevillafernandez79@gmail.com>
2017-11-02 18:52:38 -0500

    First commit y creacion  UI de Login

---
+++ src/pages/login/login.ts
@@ -0,0 +1,54 @@
+import { Component } from "@angular/core";
+import { HomePage } from '../home/home';
+import { MyApp } from '../../app/app.component';
+
+import {
+  AlertController,
+  LoadingController,
+  NavController
+} from "ionic-angular";
+
+@Component({
+  selector: "page-login",
+  templateUrl: "login.html"
+})
+export class LoginPage {
+  user = {
+    username: "",
+    password: ""
+  };
+
+  constructor(
+    private alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
+    public navCtrl: NavController
+  ) {}
+
+  ngOnInit() {
+    console.log("Arrancó el init");
+  }
+
+  login = (): void => {
+    if (this.user.username != "" && this.user.password != "") {
+      let loading = this.loadingCtrl.create({
+        content: "Please wait..."
+      });
+      loading.present();
+      setTimeout(() => {
+        loading.dismiss();
+        this.navCtrl.push(HomePage);
+      }, 3000);
+    } else {
+      let alert = this.alertCtrl.create({
+        title: "Login",
+        subTitle: "Usuario y/o contraseña invalida.",
+        buttons: ["Aceptar"]
+      });
+      alert.present();
+    }
+  };
+
+  signIn = (): void => {
+    alert("signIn");
+  };
+}
